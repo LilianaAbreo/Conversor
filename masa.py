26013526262006e4bcb079aa1146fee98bafd29e
Liliana Abreo <abreomanosalva4@gmail.com>
2023-09-24 18:13:58 -0300

    Última versión con todos los módulos y el convertidor

---
+++ masa.py
@@ -0,0 +1,14 @@
+def kilogram_a_gram(kilogram):
+    return kilogram * 1000
+
+def kilogram_a_ton(kilogram):
+    return kilogram / 1000
+
+def gram_a_kilogram(gram):
+    return gram / 1000
+
+def gram_a_ton(gram):
+    return gram / 1000000
+
+def ton_a_kilogram(ton):
+    return ton * 1000
